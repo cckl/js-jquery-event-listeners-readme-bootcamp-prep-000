ba6faba6fcc972a0e3df7b7b9261291574c6d83f
Christina L <clingc7@gmail.com>
2018-07-30 18:35:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form#typing').on('keydown', function() {
+  $('form').on('keydown', function() {
     if (key.which == 71) {
       alert('You pressed G!');
     }
