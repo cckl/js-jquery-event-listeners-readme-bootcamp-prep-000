14dd570448c04b6fe2ca61f2c42c20cae9dfbb05
Christina L <clingc7@gmail.com>
2018-07-30 18:23:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,19 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img')
+}
+
 $(document).ready(function(){
 
 // call functions here
 
 });
 
-function getIt() {
-  $('p').on('click', function(){
-    alert("Hey!")
-  })
-}
+
