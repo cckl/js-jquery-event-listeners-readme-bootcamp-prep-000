cd98d1579146f27cfc25bdec3e6be152de4f194c
Christina L <clingc7@gmail.com>
2018-07-30 18:29:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,20 +2,26 @@
 
 function getIt() {
   $('p').on('click', function(){
-    alert("Hey!")
-  })
+    alert("Hey!");
+  });
 }
 
 function frameIt() {
   $('img').on('load', function() {
-    this.classList.add('.tasty')
-  })
+    this.classList.add('.tasty');
+  });
 }
 
 function pressIt() {
   $('form#typing').on('keydwn', function() {
-    if (key.which === )
-  })
+    if (key.which === 71) {
+      alert("You pressed G!");
+    }
+  });
+}
+
+function submitIt() {
+  
 }
 
 $(document).ready(function(){
